764697d1b1d390d81a240c6045088f9f14d5f1f3
jonathan20dev <jonathan20.dev@gmail.com>
2022-09-11 09:35:17 -0600

    chat ON

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {AuthProvider} from "./context/auth";
+import {PrivateRoute} from "./components/PrivateRoute";
+import {Navbar} from "./components/Navbar"
+import {Home} from "./pages/Home"
+import { Register } from "./pages/Register";
+import {Login} from "./pages/Login"
+import {Profile} from "./pages/Profile"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <BrowserRouter>
+          <Navbar/>
+            <Routes>
+            <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
+            <Route path="/" element={<PrivateRoute><Home /></PrivateRoute>} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
